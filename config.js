61f9c1c6fd040fc602406107effe777950a88312
Javier Arce <javierarce@users.noreply.github.com>
2019-06-17 09:19:48 +0200

    Updates start message

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const TEXTS = {
-  start: 'Hola, {u}, ¡bienvenido/a a Surf Club!\n\nAquí tienes el código de conducta de este evento: https://github.com/incompleto/code-of-conduct\n\nEn este canal podrás compartir los enlaces que quieras mientras nuestras cuatro surferas navegan por la red 🤙\n\n Escribe /help para ver una lista de comandos disponibles',
+  start: 'Hola, {u}, ¡bienvenido/a a Surf Club!\n\nAquí tienes el código de conducta de este evento: https://github.com/incompleto/code-of-conduct\n\nComparte conmigo los enlaces que quieras mientras nuestras cuatro surferas navegan por la red y los mostraré en la pantalla de la sala 🤙\n\n Escribe /help para ver una lista de comandos disponibles',
   commands: '/start - mensaje inicial\n/ping - llamada de atención en la pantalla'
 }
 
